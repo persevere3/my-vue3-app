7f1b4e22cf64b2f90f4246101e8f588b2045c99f
AE-JACOB\Jacob <psves0512@gmail.com>
2025-07-30 18:04:43 +0800

    ing

--- src/router/constantRoutes.ts
+++ src/router/constantRoutes.ts
@@ -4,18 +4,18 @@ export const constantRoutes: RouteRecordRaw[] = [
   {
         path:"/",
         name:"Home",
-        component:()=>import("@/layouts/Default")
+        component:()=>import("@/layouts/Default/index.vue")
   },
   {
       path: '/login',
       name: 'Login',
-      component: () => import("@/views/Login"),
+      component: () => import("@/views/Login/index.vue"),
       meta: { title: '登入', public: true },
   },
   {
       path: '/:pathMatch(.*)*',
       name: 'NotFound',
-      component: () => import("@/views/NotFound"),
+      component: () => import("@/views/NotFound/index.vue"),
       meta: { title: '找不到頁面', public: true },
   },
 ]
